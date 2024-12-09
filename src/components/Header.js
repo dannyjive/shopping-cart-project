63b4dcfd127787b170a8b330f81809bd98bc0442
Joshua Atendido Bear <joshuabear87@gmail.com>
2024-12-08 19:37:59 -0800

    styling just a little

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { Navbar, NavbarBrand } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
 
 const Header = () => {
   return (
-    <div>
-        <Navbar className='bg-dark text-light'>
-            <NavbarBrand>
+    <>
+        <Navbar className='navbar justify-content-between p-3'>
+            <NavbarBrand className='navbar-brand fs-2 p-0 m-0'>
                 Our Shopping Cart App
             </NavbarBrand>
+            <ul>
+                <li>
+                    <a href='#'>sign in</a>
+                </li>
+                <li>
+                    <a href='#'>shopping cart</a>
+                </li>
+            </ul>
         </Navbar>
-    </div>
+        <div className='menu-div d-flex justify-content-center gap-5'>
+            <a href='#'>menu</a>
+            <a href='#'>menu</a>
+            <a href='#'>menu</a>
+            <a href='#'>menu</a>
+        </div>
+    </>
   )
 }
 
